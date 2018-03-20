92ba44eabc68a285b938e6ec1b1320bb1d21fe62
Chaim Reisman <crreisman@gmail.com>
2018-03-20 14:04:06 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,8 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/name' do
-    "Chaim Reisman"
+    "My name is Chaim Reisman"
   end 
   
-  get 
+  get '/hometown' do 
+    "My hometown is Brooklyn"
+  end 
+  
+  get '/favorite song' do
+    "My favorite song is How you remind me."
+  end
 end
